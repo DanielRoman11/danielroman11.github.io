45c01177d4db884698c9f5878cf6a081391f0034
Daniel Román <dordone46758@universidadean.edu.co>
2022-08-05 00:47:20 -0500

    Update ('Display: Grid; and grid order')

--- src/index.js
+++ src/index.js
@@ -8,14 +8,18 @@ const app = express()
 
 //Trayendo la ruta absoluta con dirname y fileURLToPath
 const __dirname = dirname(fileURLToPath(import.meta.url))
-console.log(join(__dirname, 'views')); //--> Así es como se ve la ruta absoluta puesta en el setter
+// console.log(join(__dirname, 'views')); //--> Así es como se ve la ruta absoluta puesta en el setter
 // Setters
 app.set("views", join(__dirname, 'views')) //join se utiliza para que el slash sea el adecuado dependiendo del sistema operativo
 app.set("view engine", "ejs" )
 
+app.set(express.static(join(__dirname, 'images')))
 app.use(indexRoutes) //utilizando el rutero de la carpeta /routes
 
 app.use(express.static(join(__dirname, 'public')))
+// console.log(('public','/images')); //prueba: Vista de imágenes con nombres
+
+
 
 app.listen(3000)
 console.log('Servidor en el puerto', 3000);
